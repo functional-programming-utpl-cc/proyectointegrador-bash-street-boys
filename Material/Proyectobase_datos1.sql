e6a580e0bb904473dfd06cb11942d3752602a63e
dani117m <marcelodaniel-117@hotmail.com>
2020-01-29 17:05:57 -0500

    La tabla se esta mejorando para poder crear de buena manera los datos usuarios

--- Material/Proyectobase_datos1.sql
+++ Material/Proyectobase_datos1.sql
@@ -15,10 +15,45 @@ from AUXILAR2;
 commit;
 
 select from_user_id_str, count(*) from auxilar2 group by from_user_id_str;
-  /*  
 
-INSERT INTO Usuario( from_user_id_str, from_user ,user_lang, profile_image_url, user_followers_count,
-                    user_friends_count, user_location)
-SELECT DISTINCT from_user_id_str, from_user ,user_lang, profile_image_url, user_followers_count,
-       user_friends_count, user_location FROM ODS_2_3_COPIA;
-       */
+/*
+drop table TWEET;
+CREATE TABLE TWEET(
+    text VARCHAR(280),
+    created_at Date,
+    creat_at DATE,
+    time DATE,
+    geo_cordinates NUMBER (38, 0)
+    in_reply_to_user_id_str CHAR,
+    in_reply_to_user_id_str CHAR);
+// SE crea una tabla auxiliar donde se selecion solo los fron_user_id 
+solo va a selecionar a los mas actuales, se hace un selec distyn, referenciando a la tabla donde estas ods totlmente limpio
+luego se compara con la tabla ausiliar2 con la tabla original obs, donde la fecha sea igual a la fecha creada anterior cojera los que importan
+
+);
+/
+
+   
+    
+    CREATE TABLE auxliar2  as
+(
+    SELECT from_user_id_str,MAX (to_date(substr(time, 1, 20), 'DD-MM-RR HH24:MI:SS'))  as fecha FROM AUXILIAR group by (FROM_USER_ID_STR)
+);
+
+select DISTINCT A.FROM_USER_ID_STR, A.from_user, A.profile_image_url, A.user_followers_count, A.user_friends_count, A.user_location
+from AUXILIAR A, aux
+     WHERE aux.FROM_USER_ID_STR = A.from_user_id_str
+        and aux.fecha = to_date(substr(time, 1, 20), 'DD-MM-RR HH24:MI:SS') ;
+
+
+CREATE TABLE USUARIO (
+    FROM_USER_ID_STR INT,
+    FROM_USER VARCHAR2(60),
+    USER_LANG VARCHAR2(10),
+    PROFILE_IMAGE_URL VARCHAR2(250),
+    USER_FOLLOWERS_COUNT INT,
+    USER_FRIENDS_COUNT INT,
+    USER_LOCATION VARCHAR2(200),
+    CONSTRAINT FROM_USER_ID_STR_PK PRIMARY KEY (FROM_USER_ID_STR)
+);
+ */
